c96ddf6dcf18e54b4793161496b8e51b79005294
jejones24 <jaxonejones605@gmail.com>
2025-07-14 10:42:35 -0500

    Delete src directory

--- src/functions/New-Document.jsx
+++
@@ -1,7 +0,0 @@
-function newDocument(str) {
-    /** Will read the text provided by the API and write a file. At the moment, this will only be a .txt file, but
-     * will expand to include more file types down the road. After the new file is created, it will create a hyperlink
-     * on the given panel corresponding to the proper assessment or assignment.
-     */
-
-}
